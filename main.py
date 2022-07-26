ccf0d3f48cb4ac15f97a131c5042c3872acc5748
Aishwarya Prabhat <aish.prabhat@shopee.com>
2022-07-26 09:55:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ from sklearn.ensemble import RandomForestClassifier
 import seaborn as sns
 import numpy as np
 
+
+###########################
+     # Load Data #
+###########################
 print("\nLoading data.....")
 data = datasets.load_digits().images.reshape((len(datasets.load_digits().images), -1))
 X_train, X_test, y_train, y_test = train_test_split(
     data, datasets.load_digits().target, test_size=0.5, shuffle=False
 )
 
+###########################
+    # Train and Predict #
+###########################
 print("\nTraining and predicting using Decision Tree Classifier.....")
 clf = DecisionTreeClassifier()
 clf.fit(X_train, y_train)
@@ -22,6 +29,9 @@ print(
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
 
+###########################
+    # Train and Predict #
+###########################
 print("\nTraining and predicting using Random Forest Classifier.....")
 clf = RandomForestClassifier()
 clf.fit(X_train, y_train)
