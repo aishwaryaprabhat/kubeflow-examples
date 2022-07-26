124fc5da981051755de339aee3eb4ab8eddadfac
Aishwarya Prabhat <aish.prabhat@shopee.com>
2022-07-26 08:23:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def print_pattern(rows=10):
-    for i in range(rows):
-        for j in range(i):
-            print(i, end=' ')
-        print('')
+import matplotlib.pyplot as plt
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
+import numpy as np
 
-if __name__ == "__main__":
-    print_pattern()
+print("\nLoading data.....")
+data = datasets.load_digits().images.reshape((len(datasets.load_digits().images), -1))
+X_train, X_test, y_train, y_test = train_test_split(
+    data, datasets.load_digits().target, test_size=0.5, shuffle=False
+)
+
+print("\nTraining and predicting using Decision Tree Classifier.....")
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
+
+print("\nTraining and predicting using Random Forest Classifier.....")
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
