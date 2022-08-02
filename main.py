c487f9e3603a56cbbef3b1262d7fa56cd61bf9a8
Aishwarya Prabhat <aish.prabhat@shopee.com>
2022-08-02 13:26:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import seaborn as sns
 import numpy as np
 
 
+digits = datasets.load_digits()
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, label in zip(axes, digits.images, digits.target):
+    ax.set_axis_off()
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title("Training: %i" % label)
+plt.show()
+
 ###########################
      # Load Data #
 ###########################
@@ -40,3 +49,16 @@ print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
+
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, prediction in zip(axes, X_test, predicted):
+    ax.set_axis_off()
+    image = image.reshape(8, 8)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title(f"Prediction: {prediction}")
+plt.show()
+
+cf_matrix = confusion_matrix(y_test, predicted)
+sns.heatmap(cf_matrix, annot=True)
+plt.show()
